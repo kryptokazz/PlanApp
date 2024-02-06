45288b9faf7d1f20b3bf7c8647f8da31c1603ff8
Caolan Leatham-Orrell <c.leathamorrell@uqconnect.edu.au>
2024-02-06 22:54:05 +1000

    Update creditFormula.py

--- creditFormula.py
+++ creditFormula.py
@@ -25,36 +25,37 @@ root = customtkinter.CTk()
 root.geometry("500x400")
 
 
-
+root.grid_columnconfigure(0, weight=1)
 
 
 cost_label =   customtkinter.CTkLabel(root, text="Enter plan cost:", font=("Arial", 20), text_color="#FFCC70")
-cost_label.pack()
+cost_label.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
 
 cost_entry = customtkinter.CTkEntry(root)
-cost_entry.pack()
+cost_entry.grid()
 
 start_label = customtkinter.CTkLabel(root, text="Enter start date (YYYY-MM-DD):",  font=("Arial", 20), text_color="#FFCC70")
-start_label.pack()
+start_label.grid()
 
 start_entry = customtkinter.CTkEntry(root)
-start_entry.pack()
+start_entry.grid()
 
 end_label = customtkinter.CTkLabel(root, text="Enter end date (YYYY-MM-DD):", font=("Arial", 20), text_color="#FFCC70" )
 
-end_label.pack()
+end_label.grid()
 end_entry = customtkinter.CTkEntry(root)
-end_entry.pack()
+end_entry.grid()
 
 calculate_button = customtkinter.CTkButton(root, text="Calculate", command=credit, corner_radius=32, fg_color="#C850C0", hover_color="#4158D0", border_color="#FFCC70", border_width=4)
 
-calculate_button.pack()
+calculate_button.grid()
 
 
 formula_label = customtkinter.CTkLabel(root, text="")
-formula_label.pack()
+formula_label.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
 
 difference_label = customtkinter.CTkLabel(root, text="")
-difference_label.pack()
+difference_label.grid()
 
 root.mainloop()
+
